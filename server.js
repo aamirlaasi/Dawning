3971507b0628acc9b5ff7f617bafc03c646b7959
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-10-10 18:50:55 -0500

    updated server.js file to include sequelize package

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-
-// DEPENDENCIES
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
 // SET UP EXPRESS
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// DATA PARSING
+// Requiring our models for syncing
+// var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
@@ -17,12 +25,16 @@ app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 app.use(express.static("app/public"));
 
 // ROUTES
+// =============================================================
 // require("./routes/api-routes.js")(app);
 // require("./routes/html-routes")(app);
 
 require("./public/js/square.js");
 
-// LISTENER
-app.listen(PORT, function(){
-    console.log("App is listening on PORT " + PORT);
-})
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
