29535de98ecbb979957b1110513015d338377c44
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-10-10 19:05:23 -0500

    Added comments to html-routes.js file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,24 @@
-// ROUTES FOR SENDING USERES TO VARIOUS HTML PAGES
+
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
 
 // DEPENDENCIES
+// =============================================================
 var path = require("path");
 
 // ROUTES
+// =============================================================
 module.exports = function(app){
+    
+	// Each of the below routes just handles the HTML page that the user gets sent to.
+
+	// Index route loads view.html
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/view.html"));
     });
 
+    // Add route loads add.html
     app.get("/add", function(req, res){
         res.sendFile(path.join(__dirname, "../public/add.html"));
     });
