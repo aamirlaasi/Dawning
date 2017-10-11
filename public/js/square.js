eb1bee6fc89b18a21655a4a410f84eea3b9f807f
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-10-10 20:12:00 -0500

    removed access token from square.js file

--- public/js/square.js
+++ public/js/square.js
@@ -2,7 +2,7 @@ let datafire = require('datafire');
 let squareup = require('@datafire/squareup').actions;
  
 let account = {
-  access_token: "sq0atp-YRoMJpxEclGjRK3zicLw5w",
+  access_token: "",
   refresh_token: "",
   client_id: "",
   client_secret: "",
